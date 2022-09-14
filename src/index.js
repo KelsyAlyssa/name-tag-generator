52fce099f3f4d129e5e156d0e32eb6ddbf3e6671
Kelsy <kelsyguerard@gmail.com>
2022-09-14 13:45:53 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-Hello!
 import React from "react";
 import ReactDOM from "react-dom";
-
+import NameTag from "./NameTag.js";
 import "./styles.css";
 
 const App = () => (
   <div className="App">
     <h1>Name Tag Generator</h1>
+    <NameTag />
   </div>
 );
 
