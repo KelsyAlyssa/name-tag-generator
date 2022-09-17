77656246745420697560149ba9c86450fb854466
Kelsy <109422588+KelsyAlyssa@users.noreply.github.com>
2022-09-17 15:22:17 -0400

    Delete data.js

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export const names = [
-  { person: "Kelsy", id: "001" },
-  { person: "Odin", id: "002" },
-  { person: "Tony", id: "003" },
-  { person: "George", id: "004" },
-  { person: "Donna", id: "005" }
-];
